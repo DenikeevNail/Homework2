cee4b78b545f6c6e597c1cd29854ee84c98f1d31
Наиль Деникеев <naildenikeev11@gmail.com>
2019-06-23 20:58:32 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Наиль";
+        int a = 26;
+        int t = 34;
+        if (a > 20 && a < 45) {
+
+        }
+
+        if (t > 30 || t < -20) {
+            System.out.println(name + "" + " не выходит гулять");
+        } else {
+            System.out.println(name + "" + " отправляется в гости к своему другу");
+        } if (a < 20) {
+
+        }
+            if (t > 0 || t < 28) {
+                System.out.println(name + "" + " не выходит гулять");
+                System.out.println(name + "" + " отправляется в гости к своему другу");
+            }
+        } else if (a > 45) {
+            if (t > -10 || t > 25) {
+                System.out.println(name + "" + " не выходит гулять");
+                System.out.println(name + "" + " отправляется в гости к своему другу");
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
