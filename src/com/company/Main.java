00d4be12309fe4993f6ec68a9151573d31264db2
Наиль Деникеев <naildenikeev11@gmail.com>
2019-06-24 23:18:43 +0600

    add return

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,8 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random c = new Random();
-        int randomAge = c.nextInt(107)- 0;
-        System.out.println( " Случайное число " + randomAge);
-
-
         String name = "Наиль";
+        int age = randomAge();
         int a = 18;
         int t = 34;
 
@@ -23,21 +19,28 @@ public class Main {
         } else if (a < 20) {
             if (t > 28 || t < 0) {
                 System.out.println(name + "" + " не выходит гулять");
-            }else {
+            } else {
                 System.out.println(name + "" + " отправляется в гости к своему другу");
             }
         } else if (a > 45) {
             if (t < -10 || t > 25) {
-                System.out.println(name + "" + " не выходит гулять");}
-                else {
+                System.out.println(name + "" + " не выходит гулять");
+            } else {
                 System.out.println(name + "" + " отправляется в гости к своему другу");
             }
         }
 
     }
+
+
+
+    public static int randomAge() {
+        Random c = new Random();
+        int randomAge = c.nextInt(107) - 0;
+        System.out.println(" Случайное число " + randomAge);
+        return randomAge;
+    }
 }
-
-
 
 
 
